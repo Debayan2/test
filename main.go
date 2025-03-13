e42cb2005c3a056efdb23aa62ea73518761ae72f
Debayan2 <debayanbiswal7@gmail.com>
2025-03-13 21:59:34 +0530

    Adding Main

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Println("Starting server on port 7070...")
+	http.ListenAndServe(":7070", nil)
+}
+
